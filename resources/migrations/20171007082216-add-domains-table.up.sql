a183609580c19a968426e966b7dadcef390e428c
Brad Lucas <brad@beaconhill.com>
2017-10-07 11:56:35 -0400

    Domains adding to database
    - Domains table
    - Associated queries
    - Add and select to main page
    - Tweak to header look in base.html
    - Formatting domains in grid

---
+++ resources/migrations/20171007082216-add-domains-table.up.sql
@@ -0,0 +1,5 @@
+CREATE TABLE domains
+(id SERIAL PRIMARY KEY,
+ name VARCHAR(254),
+ timestamp TIMESTAMP WITH TIME ZONE);
+
--- src/clj/ads_txt_reporter/routes/home.clj
+++ src/clj/ads_txt_reporter/routes/home.clj
@@ -1,17 +1,42 @@
 (ns ads-txt-reporter.routes.home
   (:require [ads-txt-reporter.layout :as layout]
-            [compojure.core :refer [defroutes GET]]
+            [compojure.core :refer [defroutes GET POST]]
+            [ads-txt-reporter.db.core :as db]
             [ring.util.http-response :as response]
-            [clojure.java.io :as io]))
+            [clojure.java.io :as io]
+            [struct.core :as st]))
 
-(defn home-page []
+
+(def name-schema
+  [[:name
+    st/required
+    st/string]
+    ])
+
+(defn validate-name [params]
+  (first (st/validate params name-schema)))
+
+(defn save-domain! [{:keys [params]}]
+  (if-let [errors (validate-name params)]
+    (-> (response/found "/")
+        (assoc :flash (assoc params :errors errors)))
+    (do
+      (db/save-domain! 
+       (assoc params :timestamp (java.sql.Timestamp. (.getTime (java.util.Date.)))))
+      (response/found "/"))))
+
+
+(defn home-page [{:keys [flash]}]
   (layout/render
-    "home.html" {:docs (-> "docs/docs.md" io/resource slurp)}))
+   "home.html"
+   (merge {:domains (db/get-domains)}
+          (select-keys flash [:name :errors]))))
 
 (defn about-page []
   (layout/render "about.html"))
 
 (defroutes home-routes
-  (GET "/" [] (home-page))
+  (GET "/" request (home-page request))
+  (POST "/" request (save-domain! request))
   (GET "/about" [] (about-page)))
 

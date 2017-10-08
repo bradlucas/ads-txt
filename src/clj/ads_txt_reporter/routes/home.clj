869be92fe943dddb75f14e49da29da0f0c61b82e
Brad Lucas <brad@beaconhill.com>
2017-10-08 08:21:24 -0400

    trim domain names before processing

--- src/clj/ads_txt_reporter/routes/home.clj
+++ src/clj/ads_txt_reporter/routes/home.clj
@@ -5,7 +5,9 @@
             [ring.util.http-response :as response]
             [clojure.java.io :as io]
             [struct.core :as st]
-            [clojurewerkz.urly.core :refer [url-like as-map]]))
+            [clojurewerkz.urly.core :refer [url-like as-map]]
+            [ads-txt-crawler.process :as p]
+            [ads-txt-crawler.domains :as d]))
 
 (def name-schema
   [[:name
@@ -16,25 +18,11 @@
 (defn validate-name [params]
   (first (st/validate params name-schema)))
 
-
-;; ----------------------------------------------------------------------------------------------------
-;; These routines can and should be pulled out from ads-txt-crawler
-;; Added here for a first pass
-(defn strip-www
-  "Remove preceeding www. from url"
-  [domain]
-  (if domain
-    (let [[a b] (clojure.string/split domain #"^www.")]
-      (if b
-        b
-        a))))
-
 (defn hostname
   "Parse url into components and return hostname"
   [url]
-  (if-let [hostname (:host (as-map (url-like url)))]
-    (strip-www hostname)))
-
+  (if-let [hostname (:host (as-map (url-like (clojure.string/trim url))))]
+    (d/strip-www hostname)))
 
 (defn save-domain! [{:keys [params]}]
   (if-let [hostname (hostname (:name params))]
@@ -48,43 +36,36 @@
             (catch java.lang.Exception e
               ;; ignore duplicate entries
               ))
-          (response/found "/domains"))
-        )
-      )
-    (response/found "/domains")
-    )
-  )
+          hostname)))))
 
+(defn crawl-domain-save [domain-name]
+  (let [id (db/get-domain-id {:name domain-name})
+        data (p/process domain-name)]
+    (doseq [d data]
+      (try
+        (db/save-record! {:domain_id (:id id)
+                          :exchange_domain (:exchange-domain d)
+                          :seller_account_id (:account-id d)
+                          :account_type (:account-type d)
+                          :tag_id (:tag-id d)
+                          :comment (:comment d)})
+        (catch java.lang.Exception e
+          ;; ignore duplicate entries
+          )))))
+
+(defn process-domain! [request]
+  ;; save the domain to the databse first
+  (if-let [hostname (save-domain! request)]
+    ;; craw the domain
+    (crawl-domain-save hostname))
+    ;; show domains page
+  (response/found "/domains"))
 
 (defn domains-page [{:keys [flash]}]
   (layout/render
    "domains.html"
    (merge {:domains (db/get-domains)}
           (select-keys flash [:name :errors :message]))))
-
-
-
-(defn save-record! [{:keys [params]}]
-  ;; (if-let [hostname (hostname (:name params))]
-  ;;   (let [params (assoc params :name hostname)]
-  ;;     (if-let [errors (validate-name params)]
-  ;;       (-> (response/found "/records")
-  ;;           (assoc :flash (assoc params :errors errors)))
-  ;;       (do
-  ;;         (try
-  ;;           (db/save-record! params)
-  ;;           (catch java.lang.Exception e
-  ;;             ;; ignore duplicate entries
-  ;;             ))
-  ;;         (response/found "/records"))
-  ;;       )
-  ;;     )
-  ;;   (response/found "/records")
-  ;;   )
-
-  (response/found "/records")
-  )
-
 
 (defn records-page [{:keys [params]}]
   (layout/render
@@ -101,27 +82,33 @@
 (defn about-page []
   (layout/render "about.html"))
 
-(defn test []
-  (try
-      (db/save-record! {:domain_id 7
-                 :exchange_domain "exchange-domain"
-                 :seller_account_id "seller-account-id"
-                 :account_type "account-type"
-                 :tag_id "tagid"
-                        :comment "this is a comment"})
-      (catch java.lang.Exception e
-        ;; ignore duiplicates
-        ))
-  (response/found "/domains"))
+;; (defn test []
+;;   (try
+;;       (db/save-record! {:domain_id 7
+;;                         :exchange_domain "exchange-domain"
+;;                         :seller_account_id "seller-account-id"
+;;                         :account_type "account-type"
+;;                         :tag_id "tagid"
+;;                         :comment "this is a comment"})
+;;       (catch java.lang.Exception e
+;;         ;; ignore duplicates
+;;         ))
+;;   (response/found "/domains"))
+
+;; (defn test2 []
+;;   (let [domain "businessinsider.com"]
+;;     (save-domain! {:params {:name domain}})
+;;     (crawl-domain-save domain))
+;;   (response/found "/domains"))
 
 
 (defroutes home-routes
   (GET "/" [] (home-page))
   (GET "/domains" request (domains-page request))
-  (POST "/domains" request (save-domain! request))
+  (POST "/domains" request (process-domain! request))
   (GET "/records" request (records-page request))
-  (POST "/records" request (save-domain! request))
   (GET "/about" [] (about-page))
-  (GET "/test" [] (test))
+  ;; (GET "/test" [] (test))
+  ;; (GET "/test2" [] (test2))
   )
 

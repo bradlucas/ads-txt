75b14e1a3b7f6253b9cc09b9b3c60ab9247b4212
Brad Lucas <brad@beaconhill.com>
2017-11-07 15:34:26 -0500

    filter non-data records

--- src/clj/ads_txt/routes/home.clj
+++ src/clj/ads_txt/routes/home.clj
@@ -40,8 +40,10 @@
           hostname)))))
 
 (defn crawl-domain-save [domain-name]
+  (println "crawl-domain-save")
   (let [id (db/get-domain-id {:name domain-name})
-        data (:records (c/get-data domain-name))]
+        records (:records (c/get-data domain-name))
+        data (filter (fn [r] (and (not-empty (:account-id r)) (not-empty (:account-id r)))) records)]
     (doseq [d data]
       (try
         (db/save-record! {:domain_id (:id id)
